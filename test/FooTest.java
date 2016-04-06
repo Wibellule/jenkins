0670df711aebd5e6b3e3618c21e58fc0f4660094
Olek Lamża <olek.lamza@gmail.com>
2016-04-06 15:16:07 +0200

    Powrót testów

---
+++ test/FooTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import jenkinstest.JenkinsTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author olek
+ */
+public class FooTest {
+    
+    public FooTest() {
+    }
+    
+       @Test
+    public void simpleFooTest() {
+        assertEquals(-5, JenkinsTest.foo(5));
+    }
+
+    @Test
+    public void negativeFooTest() {
+        assertEquals(13, JenkinsTest.foo(-13));
+    }
+
+    
+}
